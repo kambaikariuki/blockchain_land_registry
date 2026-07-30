1b9379c72867039d1c6922630e9da5c8ff05c811
Joseph Kambai <kariukikambai@gmail.com>
2026-07-30 14:44:50 +0300

    Implemented transfer ownership approval

--- test/requestTransfer.test.js
+++ test/requestTransfer.test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("LandRegistry", function () {
+describe("Transfer Requests", function () {
     let registry;
     let admin;
     let owner;
@@ -15,36 +15,6 @@ describe("LandRegistry", function () {
         registry = await LandRegistry.deploy(admin.address);
 
         await registry.waitForDeployment();
-    });
-
-    // Property Registration tests
-
-    it("should give deployer registrar role", async function () {
-
-        const role = await registry.REGISTRAR_ROLE();
-
-        expect(await registry.hasRole(role, admin.address)).to.equal(true);
-    });
-
-    it("should register a property", async function () {
-
-        await registry.registerProperty(1001,owner.address);
-
-        const property = await registry.getProperty(1001);
-
-        expect(property[0]).to.equal(1001n);
-        expect(property[1]).to.equal(owner.address);
-        expect(property[3]).to.equal(1n);
-        
-    });
-
-    it("shoud reject duplicate registration of parcel", async function () {
-
-        await registry.registerProperty(1001, owner.address);
-
-        await expect(
-            registry.registerProperty(1001, owner.address)
-        ).to.be.reverted;
     });
 
     // Transfer requests tests
